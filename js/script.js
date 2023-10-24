b41c532e1cd0fcbd33d86da01286bc12214e3fb6
Guilherme <guilhermesantanaa00@outlook.com>
2023-10-24 11:46:21 -0300

    acrescentando layout de cards

--- js/script.js
+++ js/script.js
@@ -7,14 +7,14 @@ function headerColor() {
   if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
 
     navHeader.style.backgroundColor = 'var(--first-color)';
-    navHeader.style.height = '70px';
-    navHeader.style.lineHeight = '70px';
+    navHeader.style.height = '90px';
+    navHeader.style.lineHeight = '90px';
 
   }else{
 
     navHeader.style.backgroundColor = 'transparent';
-    navHeader.style.height = '90px';
-    navHeader.style.lineHeight = '90px';
+    navHeader.style.height = '110px';
+    navHeader.style.lineHeight = '110px';
 
   }
 }
